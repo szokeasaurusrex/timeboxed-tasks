6e260f89b1b025679ebfc1a898538aac73cc734f
Daniel Szoke <daniel.szoke@sentry.io>
2024-08-19 11:43:30 +0200

    Initial Commit

---
+++ timeboxed tasks/timeboxed_tasksApp.swift
@@ -0,0 +1,32 @@
+//
+//  timeboxed_tasksApp.swift
+//  timeboxed tasks
+//
+//  Created by Daniel Szoke on 19.08.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct timeboxed_tasksApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ timeboxed tasksUITests/timeboxed_tasksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  timeboxed_tasksUITestsLaunchTests.swift
+//  timeboxed tasksUITests
+//
+//  Created by Daniel Szoke on 19.08.2024.
+//
+
+import XCTest
+
+final class timeboxed_tasksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
